b007bd6b54b631e697207fb66caa9d3025c4fa42
Andrey Nesterov <d32f123@yandex.ru>
2018-10-20 14:45:58 +0300

    Create project structure, add Spring Boot

---
+++ learnera-backend/src/main/java/com/netcracker/learnera/LearneraApplication.java
@@ -0,0 +1,12 @@
+package com.netcracker.learnera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearneraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearneraApplication.class, args);
+    }
+}
